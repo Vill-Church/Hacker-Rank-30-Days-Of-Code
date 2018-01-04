0ef52e6d4182aee668bbbd1cc5ca2b8524e03405
William Spires <will@williamspires.com>
2018-01-04 12:58:41 +0000

    Day 16 Commit

---
+++ Day 16 Exceptions - String to Integer/Day 16 Exceptions - String to Integer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_16_Exceptions___String_to_Integer
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            string S = Console.ReadLine();
+            try
+            {
+                int number = int.Parse(S);
+                Console.WriteLine(number);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Bad String");
+            }
+        }
+    }
+}
