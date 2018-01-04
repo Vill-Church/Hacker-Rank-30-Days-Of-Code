f76b23bfd95e2778dc35a882084c46b918cdaea2
William Spires <will@williamspires.com>
2018-01-04 12:27:20 +0000

    Day 2 Commit

---
+++ Day 2 Operators/Day 2 Operators/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2_Operators
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            double mealCost = double.Parse(Console.ReadLine());
+            double tipPercent = double.Parse(Console.ReadLine());
+            double taxPercent = double.Parse(Console.ReadLine());
+            double tip = mealCost * (tipPercent / 100);
+            double tax = mealCost * (taxPercent / 100);
+            double Cost = mealCost + tip + tax;
+            double totalCost = Math.Round(Cost);
+            Console.WriteLine("The total meal cost is " + totalCost + " dollars.");
+        }
+    }
+}
