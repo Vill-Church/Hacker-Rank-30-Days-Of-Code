44f8dd6cc83c5c5ec8a65864bddd3ac50c18c85c
William Spires <will@williamspires.com>
2018-01-04 12:35:29 +0000

    Day 8 Commit

---
+++ Day 8 Dictionaries and Maps/Day 8 Dictionaries and Maps/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_8_Dictionaries_and_Maps
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var phoneBook = new Dictionary<string, int>();
+
+            for (var i = 0; i < n; i++)
+            {
+                var entry = Console.ReadLine().Split(' ');
+                var name = entry[0];
+                var phone = int.Parse(entry[1]);
+                phoneBook.Add(name, phone);
+            }
+
+            for (var i = 0; i < n; i++)
+            {
+                var name = Console.ReadLine();
+                if (phoneBook.ContainsKey(name))
+                {
+                    var phone = phoneBook[name];
+                    Console.WriteLine($"{name}={phone}");
+                }
+                else Console.WriteLine("Not found");
+            }
+        }
+    }
+}
