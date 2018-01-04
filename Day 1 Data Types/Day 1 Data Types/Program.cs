89cb3deedba6690bbf27ff7b63c8affc8f74fbd3
William Spires <will@williamspires.com>
2018-01-04 12:26:10 +0000

    Day 1 Commit

---
+++ Day 1 Data Types/Day 1 Data Types/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_1_Data_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 4;
+            double d = 4.0;
+            string s = "HackerRank ";
+            int i2 = int.Parse(Console.ReadLine());
+            double d2 = double.Parse(Console.ReadLine());
+            string s2 = Console.ReadLine();
+            Console.Out.WriteLine(i + i2);
+            Console.Out.WriteLine((d + d2).ToString("0.0"));
+            Console.Out.WriteLine(s + s2);
+        }
+    }
+}
