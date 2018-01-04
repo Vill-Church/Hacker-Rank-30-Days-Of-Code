375ecebc0633db60cdbde1db4d69c0d666c21c4e
William Spires <will@williamspires.com>
2018-01-04 12:28:33 +0000

    Day 3 Commit

---
+++ Day 3 Intro to Conditional Statements/Day 3 Intro to Conditional Statements/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_3_Intro_to_Conditional_Statements
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            if (N % 2 > 0)
+            {
+                Console.WriteLine("Weird");
+            }
+            else if (N < 6 || N > 20)
+            {
+                Console.WriteLine("Not Weird");
+            }
+            else
+            {
+                Console.WriteLine("Weird");
+            }
+        }
+    }
+}
