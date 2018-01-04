dd46dd6058ca6cd907f1bf3130ed3120542fe04c
William Spires <will@williamspires.com>
2018-01-04 13:47:44 +0000

    Day 20 Commit

---
+++ Day 20 Sorting/Day 20 Sorting/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_20_Sorting
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] a_temp = Console.ReadLine().Split(' ');
+            int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+            // Write Your Code Here
+            int numSwaps = 0;
+            bool swapped = false;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n - 1; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        int temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                        swapped = true;
+                        numSwaps++;
+                    }
+                }
+                if (!swapped)
+                {
+                    i = n;
+                }
+            }
+            Console.WriteLine("Array is sorted in " + numSwaps + " swaps.");
+            Console.WriteLine("First Element: " + a[0]);
+            Console.WriteLine("Last Element: " + a[n - 1]);
+        }
+    }
+}
