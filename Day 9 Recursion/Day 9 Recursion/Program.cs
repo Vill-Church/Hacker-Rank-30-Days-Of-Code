d134f0a7883e0506ed93b114fd57357a203fef60
William Spires <will@williamspires.com>
2018-01-04 12:36:29 +0000

    Day 9 Commit

---
+++ Day 9 Recursion/Day 9 Recursion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_9_Recursion
+{
+    class Program
+    {
+        static int factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * factorial(n - 1);
+            }
+        }
+
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int result = factorial(n);
+            Console.WriteLine(result);
+        }
+    }
+}
