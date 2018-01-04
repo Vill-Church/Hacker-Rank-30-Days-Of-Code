b5cd1d17a2a244638a6fe83f7d04a098d220e945
William Spires <will@williamspires.com>
2018-01-04 12:33:00 +0000

    Day 6 Commit

---
+++ Day 6 Let's Review/Day 6 Let's Review/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_6_Let_s_Review
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int n = Int16.Parse(Console.ReadLine());
+            for (int x = 0; x < n; x++)
+            {
+                String word = Console.ReadLine();
+                for (int i = 0; i < word.Length; i += 2)
+                {
+                    Console.Write(word[i]);
+                }
+                Console.Write(" ");
+                for (int j = 1; j < word.Length; j += 2)
+                {
+                    Console.Write(word[j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
