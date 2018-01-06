69b04b3d909e28dc53605039fa331965fa9d0423
William Spires <will@williamspires.com>
2018-01-06 15:23:42 +0000

    Day 25 Commit

---
+++ Day 25 Running Time and Complexity/Day 25 Running Time and Complexity/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_25_Running_Time_and_Complexity
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+            int T = int.Parse(Console.ReadLine());
+            for (int i = 0; i < T; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                if (n > 2)
+                {
+                    if (isPrime(n) == true)
+                    {
+                        Console.WriteLine("Prime");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not prime");
+                    }
+                }
+                else if (n == 2)
+                {
+                    Console.WriteLine("Prime");
+                }
+                else
+                {
+                    Console.WriteLine("Not prime");
+                }
+
+            }
+
+        }
+        public static bool isPrime(int n)
+        {
+            int nSqrt = (int)Math.Round(Math.Sqrt(n));
+            for (int i = 2; i <= nSqrt; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
