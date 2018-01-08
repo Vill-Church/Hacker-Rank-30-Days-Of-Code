c0f59c81d0f19ff46b93439d39f2ad6d064dcf41
William Spires <will@williamspires.com>
2018-01-08 13:06:49 +0000

    Day 27 Commit

---
+++ Day 27 Testing/Day 27 Testing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_27_Testing
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("5");
+            Console.WriteLine("6 5");
+            Console.WriteLine("-3 0 3 2 0 -1");
+            Console.WriteLine("8 1");
+            Console.WriteLine("-1 0 2 1 5 -3 -2 0");
+            Console.WriteLine("3 2");
+            Console.WriteLine("0 1 2");
+            Console.WriteLine("2 2");
+            Console.WriteLine("-1 0");
+            Console.WriteLine("4 3");
+            Console.WriteLine("-1 0 2 5");
+        }
+    }
+}
